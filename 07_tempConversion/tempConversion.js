cef5d59c3efc36a8c1eead5551077c5e87e79ae7
Max Deibel <deibel.max@gmail.com>
2022-05-15 09:51:28 -0700

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  return Math.round((5 / 9 * (fTemp - 32)) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  return Math.round((9 / 5 * cTemp + 32) * 10) / 10;
 };
 
 // Do not edit below this line
