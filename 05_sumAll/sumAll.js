8a6a91fe420400452073c453c3646c4251ed6f29
Max Deibel <deibel.max@gmail.com>
2022-05-15 09:36:24 -0700

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if (typeof(first) !== 'number' || typeof(second) !== 'number' || 
+            first < 0 || second < 0) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (first > second) {
+        for (let i = second; i <= first; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for (let i = first; i <= second; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
