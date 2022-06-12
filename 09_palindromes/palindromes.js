b29d4930c8b1aedd931cb691c148fd84a3f5f898
Max Deibel <deibel.max@gmail.com>
2022-06-12 10:47:34 -0700

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    sanitizedStr = str.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').
+            toLowerCase().
+            replaceAll(/\s/g, '');
+    console.log(sanitizedStr);
+    reverseStr = '';
+    for (let i = sanitizedStr.length - 1; i >= 0; i--) {
+        reverseStr += sanitizedStr[i];
+    }
+    return (reverseStr === sanitizedStr);
 };
 
 // Do not edit below this line
