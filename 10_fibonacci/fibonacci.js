7adbc042e6a53c121718b3ee8f3e4e5ba82743e5
Max Deibel <deibel.max@gmail.com>
2022-06-12 10:57:16 -0700

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    if (index === 1 || index === 2) {
+        return 1;
+    }
+    
+    let prevFib = 1;
+    let currFib = 1;
+    let temp;
+    for (let i = 2; i < index; i++) {
+        temp = currFib;
+        currFib = prevFib + currFib;
+        prevFib = temp;
+    }
 
+    return currFib;
 };
 
 // Do not edit below this line
