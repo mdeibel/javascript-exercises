174cfb107e25785763ae791c5f600e9f1cddfffe
Max Deibel <deibel.max@gmail.com>
2022-06-13 21:01:54 -0700

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.reduce((prev, curr) => {
+        const prevDeathYear = prev.yearOfDeath || new Date().getFullYear();
+        const currDeathYear = curr.yearOfDeath || new Date().getFullYear();
+        const prevAge = prevDeathYear - prev.yearOfBirth;
+        const currAge = currDeathYear - curr.yearOfBirth;
+        return currAge > prevAge ? curr : prev;
+    });
 
+    return oldest;
 };
 
 // Do not edit below this line
